65352409420f96803debd3aef7d059348b9bab92
Amith <aknkum@essex.ac.uk>
2019-10-10 23:56:36 +0100

    Displays the data of the pop up to the list and saves the data in the data base. i have also connected the delegate and data source of the table view to InventoryVC. created core data context and adding the list items to the DB.

--- Herby/Herby/Controller/InventoryVC.swift
+++ Herby/Herby/Controller/InventoryVC.swift
@@ -8,49 +8,92 @@
 
 import Foundation
 import UIKit
-class InventoryVC : UIViewController {
+import CoreData
+
+class InventoryVC : UIViewController{
+    
+    var inventoryArrayList = [InventoryListEntity]()
+    
+    //This is for the Core Data Context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var inventoryAddButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+        tableView.dataSource = self
+        tableView.delegate = self
         inventoryAddButton.customFloatingActionButtonExt()
         
     }
     
     
     @IBAction func inventoryAddButtonAction(_ sender: Any) {
-        let alert =  UIAlertController(title: "Add inventory item", message: "Please insert your inventory item.", preferredStyle: .alert)
+        
+        var textField = UITextField()
+        
+        let alert =  UIAlertController(title: "Add item", message: "Please insert your inventory item.", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add", style: .default) {
             (act) in
             
+            //this is NSManger obj for every new item
+            let newItem = InventoryListEntity(context: self.context)
+            
+            if textField.text!.isEmpty != true {
+                newItem.name = textField.text!
+                self.inventoryArrayList.append(newItem)
+                self.saveData()
+            }
+            
         }
         
         alert.addTextField { (alertText) in
-            alertText.placeholder = "Please type your todo!"
-//            textField = alertText
+            alertText.placeholder = "Please type your item!"
+            textField = alertText
         }
         
         alert.addAction(action)
-        present(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: .none)
         
+        saveData()
     }
     
+    
+    func saveData() {
+            do {
+              try context.save()
+            } catch {
+                print("-----> error on saving data, \(error)")
+            }
+        tableView.reloadData()
+        print("---> \(inventoryArrayList.count)")
+    }
+    
+    
+    
 }
-extension InventoryVC : UITableViewDelegate, UITableViewDataSource {
+extension InventoryVC : UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        inventoryArrayList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TablViewCellID", for: indexPath)
+        cell.textLabel?.text = inventoryArrayList[indexPath.row].name
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.saveData()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
     
 }
-
